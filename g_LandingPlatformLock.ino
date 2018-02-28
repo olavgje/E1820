42d2db81a1dd91e6a3b9041fcd2467d5486d5319
jope1312 <35594662+jope1312@users.noreply.github.com>
2018-02-28 13:12:10 +0100

    Add files via upload

--- g_LandingPlatformLock.ino
+++ g_LandingPlatformLock.ino
@@ -1,1 +1,33 @@
+/*
+This function controls the servos on the "LandingPlatform".
+
+Description intput:
+
+int dronePosition     HIGH - Drone in position / LOW - Awaiting drone
+
+Description output:
+
+int droneLocking      HIGH - Drone locked / LOW - Drone 
+*/
+
+int droneLock(int dronePosition) {
+  int droneLocking;
+
+    if (dronePosition == HIGH){
+      delay(500);
+      landingPlatformFL.write(0);
+      landingPlatformFR.write(180);
+      landingPlatformBL.write(0); 
+      landingPlatformBR.write(180);
+      droneLocking == HIGH;
+    }
+
+    else {
+      landingPlatformFL.write(90);
+      landingPlatformFR.write(90);
+      landingPlatformBL.write(90);
+      landingPlatformBR.write(90);
+      droneLocking == LOW;
+    }
+}
 
