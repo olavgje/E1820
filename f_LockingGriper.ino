da862eb2aeccb72bbb1ba3ef646df8c0f82c835b
olavgje <35593671+olavgje@users.noreply.github.com>
2018-04-04 08:15:26 +0200

    Update f_LockingGriper.ino

--- f_LockingGriper.ino
+++ f_LockingGriper.ino
@@ -1,2 +1,25 @@
 
+/* 
+This function takes input for locking/opening the griper. 
+
+Descrition for input:
+
+griperStatus:     HIGH - Open griper / LOW - Lock griper
+          
+*/ 
+
+
+void griperLockOrOpen(int griperStatus) {
+  if (griperStatus == HIGH){
+    gripLeft.write(0);
+    gripRight.write(180);
+    delay(500);
+  }
+  if (griperStatus == LOW){
+    gripLeft.write(100);
+    gripRight.write(80);
+    delay(500);
+  }
+  
+}
 
