8b9d6c4822041cd9a4a036ca6a655ce8ab9fe511
Ørjan Røsnes <35594675+orjros@users.noreply.github.com>
2018-04-06 15:56:24 +0200

    Add files via upload

---
+++ l_OpenSlot.ino
@@ -0,0 +1,31 @@
+/*
+This program tells where it is an open slot
+
+Every analog signal from the chargers will have a max voltage = 4,2V
+
+*/
+
+/*
+
+GLOBAL
+  int charger1 = 60;            //analog pin 6
+  int charger2 = 61;            //analog pin 7
+  int charger3 = 62;            //analog pin 8
+  int charger4 = 63;            //analog pin 9
+  int charger5 = 64;            //analog pin 10
+
+SETUP
+  pinMode(charger1, INPUT);
+  pinMode(charger2, INPUT);
+  pinMode(charger3, INPUT);
+  pinMode(charger4, INPUT);
+  pinMode(charger5, INPUT);
+
+
+*/
+
+
+
+
+
+
