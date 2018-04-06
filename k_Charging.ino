87d2a12045cd181bd84633b4140b3b3558e0a4bd
olavgje <35593671+olavgje@users.noreply.github.com>
2018-04-06 15:58:08 +0200

    Update k_Charging.ino

--- k_Charging.ino
+++ k_Charging.ino
@@ -8,11 +8,11 @@ Every analog signal from the chargers will have a max voltage = 4,2V
 /*
 
 GLOBAL
-  int charger1 = 60;            //analog pin 6
-  int charger2 = 61;            //analog pin 7
-  int charger3 = 62;            //analog pin 8
-  int charger4 = 63;            //analog pin 9
-  int charger5 = 64;            //analog pin 10
+int charger1 = 60;            //analog pin 6
+int charger2 = 61;            //analog pin 7
+int charger3 = 62;            //analog pin 8
+int charger4 = 63;            //analog pin 9
+int charger5 = 64;            //analog pin 10
 
 SETUP
   pinMode(charger1, INPUT);
