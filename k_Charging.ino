125b0def5378cbb3ec83998a2a4eca7b95eb631f
olavgje <35593671+olavgje@users.noreply.github.com>
2018-05-19 14:09:46 +0200

    Update k_Charging.ino

--- k_Charging.ino
+++ k_Charging.ino
@@ -1,22 +1,8 @@
 /*
-This program chooses the most charged battery
-Every analog signal from the chargers will have a max voltage = 4,2V
+This program returns the most charged battery.
+Every analog signal from the chargers will have a max voltage = 4,2V.
 */
 
-/*
-GLOBAL
-int charger1 = 60;            //analog pin 6
-int charger2 = 61;            //analog pin 7
-int charger3 = 62;            //analog pin 8
-int charger4 = 63;            //analog pin 9
-int charger5 = 64;            //analog pin 10
-SETUP
-  pinMode(charger1, INPUT);
-  pinMode(charger2, INPUT);
-  pinMode(charger3, INPUT);
-  pinMode(charger4, INPUT);
-  pinMode(charger5, INPUT);
-*/
 
 
 int chargedBattery() {
